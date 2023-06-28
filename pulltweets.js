658269519e79edb3db42c3ad79906f31ce8cbc84
Yeite Rilsosing Koireng <53212931+chimmykk@users.noreply.github.com>
2023-06-28 22:26:25 +0530

    Update pulltweets.js

--- pulltweets.js
+++ pulltweets.js
@@ -15,7 +15,7 @@ app.get('/pulltweet/test.json', (req, res) => {
     if (err) {
       res.status(500).send('Internal Server Error');
     } else {
-    res.setHeader('Access-Control-Allow-Origin', 'https://new-dogie-z1in.vercel.app, http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
       res.status(200).json(JSON.parse(data));
     }
   });
