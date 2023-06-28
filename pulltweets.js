5691e8d76fa476d0c508e1ce9d83a55540f3e4d5
Yeite Rilsosing Koireng <53212931+chimmykk@users.noreply.github.com>
2023-06-28 20:13:30 +0530

    Update pulltweets.js

--- pulltweets.js
+++ pulltweets.js
@@ -15,6 +15,7 @@ app.get('/pulltweet/test.json', (req, res) => {
     if (err) {
       res.status(500).send('Internal Server Error');
     } else {
+      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
       res.status(200).json(JSON.parse(data));
     }
   });
