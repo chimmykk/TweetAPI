1ca7041fdf19a464515951390d9b5d747ec1d025
Yeite Rilsosing Koireng <53212931+chimmykk@users.noreply.github.com>
2023-07-01 13:20:18 +0530

    Update pulltweets.js
    update cors

--- pulltweets.js
+++ pulltweets.js
@@ -15,7 +15,7 @@ app.get('/pulltweet/test.json', (req, res) => {
     if (err) {
       res.status(500).send('Internal Server Error');
     } else {
-    res.setHeader('Access-Control-Allow-Origin', 'https://fluffy-fairy-2956d8.netlify.app');
+    res.setHeader('Access-Control-Allow-Origin', 'https://stirring-chimera-fa5b9b.netlify.app');
       res.status(200).json(JSON.parse(data));
     }
   });
