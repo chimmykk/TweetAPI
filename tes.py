05d9fd59b33268374110940db8ed7b4df5a8acb3
Rilso <53212931+chimmykk@users.noreply.github.com>
2023-07-17 23:40:27 +0530

    Update tes.py

--- tes.py
+++ tes.py
@@ -1,46 +1,88 @@
 import subprocess
 import time
 import pyautogui
+import requests
 
-def automate_chatbot():
-    # Open the chatbot application
-    subprocess.Popen(r'C:\Users\paperspace\Downloads\AllCharactersAI_v0.18\AllCharactersAI_v0.18\Windows\Chatbot_Characters.exe')
+# Set bearer token
+bearer_token = "AAAAAAAAAAAAAAAAAAAAACzGoQEAAAAAPBUyxZyXrtNzab8B3guBR9e994k%3DDieBbwNezSIZ26haV9I17LcWMpOf2z7nPkC9gYKFJEwL0AnTF3"
 
-    time.sleep(2)
+# Set endpoint URL
+endpoint_url = "https://api.twitter.com/2/tweets/search/recent"
 
-    # Press "Tab" key three times to navigate to the Doge option
-    pyautogui.press('tab')
-    pyautogui.press('tab')
-    pyautogui.press('tab')
+def get_request():
+    # Edit query parameters below
+    # Specify a search query, and any additional fields that are required
+    # By default, only the Tweet ID and text fields are returned
+    params = {
+        'query': '@doge_gm -is:retweet',
+        'tweet.fields': 'author_id'
+    }
 
-    # Press "Enter" key to select the Doge option
-    pyautogui.press('enter')
+    headers = {
+        "User-Agent": "v2RecentSearchPython",
+        "Authorization": f"Bearer {bearer_token}"
+    }
 
-    time.sleep(2)
+    response = requests.get(endpoint_url, params=params, headers=headers)
+    response_json = response.json()
 
-    # Press "Tab" key three times to trigger three tabs again
-    pyautogui.press('tab')
-    pyautogui.press('tab')
-    pyautogui.press('tab')
+    if response.status_code == 200:
+        return response_json
+    else:
+        raise Exception('Unsuccessful request')
 
-    time.sleep(2)
+try:
+    # Make request
+    response = get_request()
 
-    # Type "what is your name"
-    pyautogui.typewrite("DO YOU KNOW NARENDRA MODI")
+    # Display the first recent mention with text and tweet ID
+    if 'data' in response and len(response['data']) > 0:
+        tweet = response['data'][0]
+        if 'text' in tweet and 'id' in tweet:
+            tweet_text = tweet['text']
+            tweet_id = tweet['id']
+            print("Tweet ID:", tweet_id)
+            print("Tweet Text:", tweet_text)
 
-    # Press "Enter" key to send the message
-    pyautogui.press('enter')
+            # Open the chatbot application
+            subprocess.Popen(r'C:\Users\paperspace\Downloads\AllCharactersAI_v0.18\AllCharactersAI_v0.18\Windows\Chatbot_Characters.exe')
 
-    # Add a 2-second delay
-    time.sleep(2)
+            time.sleep(2)
 
-    # Enable screen recording
-    pyautogui.hotkey('alt', 'f9')
+            # Press "Tab" key three times to navigate to the Doge option
+            pyautogui.press('tab')
+            pyautogui.press('tab')
+            pyautogui.press('tab')
 
-    # Wait for 1 minute
-    time.sleep(60)
+            # Press "Enter" key to select the Doge option
+            pyautogui.press('enter')
 
-    # Close the application
-    pyautogui.hotkey('alt', 'f4')
+            time.sleep(2)
 
-automate_chatbot()
+            # Press "Tab" key three times to trigger three tabs again
+            pyautogui.press('tab')
+            pyautogui.press('tab')
+            pyautogui.press('tab')
+
+            time.sleep(2)
+
+            # Type the generated tweet text as the message
+            pyautogui.typewrite(tweet_text)
+
+            # Press "Enter" key to send the message
+            pyautogui.press('enter')
+
+            # Add a 2-second delay
+            time.sleep(2)
+
+            # Press F12 button
+            pyautogui.press('f12')
+
+            # Wait for 1 minute
+            time.sleep(60)
+
+            # Trigger another F12 button after 1 minute
+            pyautogui.press('f12')
+
+except Exception as e:
+    print(e)
